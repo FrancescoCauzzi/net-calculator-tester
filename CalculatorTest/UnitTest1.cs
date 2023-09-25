c8d349cd0a603c232f000b411e173b066fec60d4
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-25 16:02:24 +0200

    feat: base exercise done

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,17 +1,60 @@
+using net_calculator_tester;
 namespace CalculatorTest
 {
     [TestFixture]
     public class Tests
     {
+        
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void TestDivide()
         {
-            Assert.Pass();
+            double result = Calculator.Divide(144, 12);
+            Assert.AreEqual(12, result);
         }
+
+        [Test]
+        public void TestDivideForZeroDivisor()
+        {
+            Assert.Throws<System.ArgumentException>(() => { Calculator.Divide(10, 0); },
+            "Deve sollevare IllegalArgumentException per divisione per zero");
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            double result = Calculator.Add(3, 4);  
+            Assert.AreEqual(7, result); 
+        }
+
+        [Test]
+        public void TestMultiply()
+        {
+            double result = Calculator.Multiply(3, 4);
+            Assert.AreEqual(12, result);
+        }
+        [Test]
+        public void TestMultiplyForZeroArgument() {
+            double result = Calculator.Multiply(5, 0);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void TestMultiplyOverflow()
+        {
+            double result = Calculator.Multiply((float)double.MaxValue, 2);
+            Assert.AreEqual(double.PositiveInfinity, result);
+        }
+        [Test]
+        public void TestSubtract()
+        {
+            double result = Calculator.Subtract(-12, 18);
+            Assert.AreEqual(-30, result);
+        }
+        
+
     }
 }
--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -12,7 +12,15 @@ namespace net_calculator_tester
 
         public static float Subtract(float a, float b) { return a - b; }
 
-        public static float Divide(float a, float b) { return a/b; }
+        
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new System.ArgumentException("Impossibile dividere per zero");
+            }
+            return a / b;
+        }
 
         public static float Multiply(float a, float b) { return a*b; }
 
